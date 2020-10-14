b3aef3359d12ddfdc990782328bf7ccefdb4589d
Steve Shen <golddragon11@gmail.com>
2020-10-14 12:49:30 +0800

    Initial Commit

---
+++ iOSSecondHW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSSecondHW
+//
+//  Created by Steve Shen on 2020/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSSecondHW/iOSSecondHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSecondHWApp.swift
+//  iOSSecondHW
+//
+//  Created by Steve Shen on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct iOSSecondHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
